6f60b40e2c3ee9eac6f084af68de1fc0d97dbd92
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-07-30 22:57:32 +0530

    ternary operator usage, toggle HTML classes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -30,5 +30,20 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
+
+    if (dice !== 1) {
+        roundScore += dice;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    } else {
+        roundScore = 0;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
 
 });
