7c25e0da88fa6e50f10c00c0deeef32fec4a5492
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-07-30 21:51:30 +0530

    Set up Event Handler, select element by ID, change the image in an IMG element

--- app.js
+++ app.js
@@ -10,31 +10,25 @@ GAME RULES:
 */
 
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
+document.querySelector('.btn-roll').addEventListener('click', function () {
 
+    var dice = Math.floor(Math.random() * 6) + 1;
 
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
-
-
-
-
-
-
-
-
-
-
-
-
+});
